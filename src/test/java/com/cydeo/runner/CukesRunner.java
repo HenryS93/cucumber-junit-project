f40688d50c8ae7807eee2ad4db19106e5fd2543f
Henry Sanchez <henrysanchez1993@hotmail.com>
2022-11-18 21:37:36 -0500

    json added

--- src/test/java/com/cydeo/runner/CukesRunner.java
+++ src/test/java/com/cydeo/runner/CukesRunner.java
@@ -6,7 +6,9 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"html:target/cucumber-report.html",
+        plugin = {
+                "json:target/cucumber.json",
+                "html:target/cucumber-report.html",
         "rerun:target/rerun.txt",
         "me.jvt.cucumber.report.PrettyReports:target/cucumber" },
         features = "src/test/resources/features", //  comes from "feature folder"->right click->copy path->click->path from content/root
